2e9213d3f620a5bd5a18e24cd09f2ea35111d1dc
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-05-30 16:18:42 +0300

    end
    create class and end

--- MilionerV2_1513174412/Milioners/Model/Data_first.cs
+++ MilionerV2_1513174412/Milioners/Model/Data_first.cs
@@ -7,26 +7,31 @@ using System.Data;
 
 namespace Milioners
 {
-    //class Data_first
-    //{
-    //    public static void Save_d(string q, string a1, string a2, string a3, string a4)
-    //    {
+    class Data_first
+    {
+        public static void Save_d(string q, string a1, string a2, string a3, string a4)
+        {
 
-    //        using (var db = new MilionEntities4())
-    //        {
-                
+            using (var db = new MilionEntities5())
+            {
 
-    //            var questions = new Questio { Questio1 = q,Answer_1=a1,Answer_2=a2,Answer_3=a3,Answer_4=a4 };
-    //            db.Questios.Add(questions);
-    //            db.SaveChanges();
-               
-    //        }
 
-    //    }
+                var questions = new Questio { Questio1 = q, Answer_1 = a1, Answer_2 = a2, Answer_3 = a3, Answer_4 = a4 };
+                db.Questios.Add(questions);
+                db.SaveChanges();
 
-    //    public static void Update_Questio_d(string Questio_old, string Questio, string Answer_1, string Answer_2, string Answer_3, string Answer_4)
-    //    {
+            }
 
-    //    }
-    //}
+        }
+
+        public static void Update_Questio_d(string Questio_old, string Questio, string Answer_1, string Answer_2, string Answer_3, string Answer_4)
+        {
+
+        }
+
+        public static void Delete_Questio_d(string Questio)
+        {
+        
+        }
+    }
 }
