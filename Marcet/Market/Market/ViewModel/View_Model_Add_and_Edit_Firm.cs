32f58cac8bf351bd8cfb8b834b5f88d4afa290f5
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-06-12 09:37:17 +0300

    boss

--- Marcet/Market/Market/ViewModel/View_Model_Add_and_Edit_Firm.cs
+++ Marcet/Market/Market/ViewModel/View_Model_Add_and_Edit_Firm.cs
@@ -142,15 +142,15 @@ namespace Market.ViewModel
 
             City temp = new City();
             temp.Name = Convert.ToString(city);
-            list_phone.Add(temp);
-            OnPropertyChanged(nameof(List_phone));
+            list_city.Add(temp);
+            OnPropertyChanged(nameof(List_city));
 
         }
-        private bool CanExecute_New_Phone(object o)
+        private bool CanExecute_New_City(object o)
         {
 
 
-            if (phone != null && phone.Length > 0)
+            if (city != null && city.Length > 0)
                 return true;
             else
                 return false;
@@ -159,34 +159,34 @@ namespace Market.ViewModel
         }
         #endregion
         #region 
-        private DelegateCommand _Command_Delete_Phone;
-        public ICommand Button_Delete_Phone
+        private DelegateCommand _Command_Delete_City;
+        public ICommand Button_Delete_City
         {
             get
             {
-                if (_Command_Delete_Phone == null)
+                if (_Command_Delete_City == null)
                 {
-                    _Command_Delete_Phone = new DelegateCommand(Execute_Delete_Phone, CanExecute_Delete_Phone);
+                    _Command_Delete_City = new DelegateCommand(Execute_Delete_City, CanExecute_Delete_City);
                 }
-                return _Command_Delete_Phone;
+                return _Command_Delete_City;
             }
         }
-        private void Execute_Delete_Phone(object o)
+        private void Execute_Delete_City(object o)
         {
 
-            list_phone.Remove(select_item_phone);
+            list_city.Remove(select_item_city);
 
-            OnPropertyChanged(nameof(List_phone));
-            Select_item_phone = null;
+            OnPropertyChanged(nameof(List_city));
+            Select_item_city = null;
             Phone = null;
 
 
         }
-        private bool CanExecute_Delete_Phone(object o)
+        private bool CanExecute_Delete_City(object o)
         {
 
 
-            if (select_item_phone != null)
+            if (select_item_city != null)
                 return true;
             else
                 return false;
@@ -195,31 +195,30 @@ namespace Market.ViewModel
         }
         #endregion
         #region 
-        private DelegateCommand _Command_Edit_phone;
-        public ICommand Button_Edit_Phone
+        private DelegateCommand _Command_Edit_city;
+        public ICommand Button_Edit_City
         {
             get
             {
-                if (_Command_Edit_phone == null)
+                if (_Command_Edit_city == null)
                 {
-                    _Command_Edit_phone = new DelegateCommand(Execute_Edit_phone, CanExecute_Edit_phone);
+                    _Command_Edit_city = new DelegateCommand(Execute_Edit_city, CanExecute_Edit_city);
                 }
-                return _Command_Edit_phone;
+                return _Command_Edit_city;
             }
         }
-        private void Execute_Edit_phone(object o)
+        private void Execute_Edit_city(object o)
         {
 
-            Select_item_phone.Number = Phone;
-            OnPropertyChanged(nameof(List_phone));
+            Select_item_city.Name = City;
+            OnPropertyChanged(nameof(List_city));
 
 
         }
-
-        private bool CanExecute_Edit_phone(object o)
+        private bool CanExecute_Edit_city(object o)
         {
 
-            if (select_item_phone != null && Phone.Length > 0)
+            if (select_item_city != null && City.Length > 0)
                 return true;
             else
                 return false;
@@ -228,6 +227,325 @@ namespace Market.ViewModel
         }
         #endregion
         #endregion City
+
+        #region Country
+        #region 
+        private DelegateCommand _Command_New_country;
+        public ICommand Button_New_country
+        {
+            get
+            {
+                if (_Command_New_country == null)
+                {
+                    _Command_New_country = new DelegateCommand(Execute_New_country, CanExecute_New_country);
+                }
+                return _Command_New_country;
+            }
+        }
+        private void Execute_New_country(object o)
+        {
+
+            Country temp = new Country();
+            temp.Name = Convert.ToString(country);
+            list_country.Add(temp);
+            OnPropertyChanged(nameof(List_country));
+
+        }
+        private bool CanExecute_New_country(object o)
+        {
+
+
+            if (country != null && country.Length > 0)
+                return true;
+            else
+                return false;
+
+
+        }
+        #endregion
+        #region 
+        private DelegateCommand _Command_Delete_country;
+        public ICommand Button_Delete_country
+        {
+            get
+            {
+                if (_Command_Delete_country == null)
+                {
+                    _Command_Delete_country = new DelegateCommand(Execute_Delete_country, CanExecute_Delete_country);
+                }
+                return _Command_Delete_country;
+            }
+        }
+        private void Execute_Delete_country(object o)
+        {
+
+            list_country.Remove(select_item_country);
+
+            OnPropertyChanged(nameof(List_country));
+            Select_item_country = null;
+            Country = null;
+
+
+        }
+        private bool CanExecute_Delete_country(object o)
+        {
+
+
+            if (select_item_country != null)
+                return true;
+            else
+                return false;
+
+
+        }
+        #endregion
+        #region 
+        private DelegateCommand _Command_Edit_country;
+        public ICommand Button_Edit_country
+        {
+            get
+            {
+                if (_Command_Edit_country == null)
+                {
+                    _Command_Edit_country = new DelegateCommand(Execute_Edit_country, CanExecute_Edit_country);
+                }
+                return _Command_Edit_country;
+            }
+        }
+        private void Execute_Edit_country(object o)
+        {
+
+            Select_item_country.Name = City;
+            OnPropertyChanged(nameof(List_country));
+
+
+        }
+        private bool CanExecute_Edit_country(object o)
+        {
+
+            if (select_item_country != null && country.Length > 0)
+                return true;
+            else
+                return false;
+
+
+        }
+        #endregion
+        #endregion Country
+
+        #region Adressa
+        #region 
+        private DelegateCommand _Command_New_adressa;
+        public ICommand Button_New_adressa
+        {
+            get
+            {
+                if (_Command_New_adressa == null)
+                {
+                    _Command_New_adressa = new DelegateCommand(Execute_New_adressa, CanExecute_New_adressa);
+                }
+                return _Command_New_adressa;
+            }
+        }
+        private void Execute_New_adressa(object o)
+        {
+
+            Adressa temp = new Market.Adressa();
+            temp.Name = Convert.ToString(adressa);
+            list_adressa.Add(temp);
+            OnPropertyChanged(nameof(List_adressa));
+
+        }
+        private bool CanExecute_New_adressa(object o)
+        {
+
+
+            if (adressa != null && adressa.Length > 0)
+                return true;
+            else
+                return false;
+
+
+        }
+        #endregion
+        #region 
+        private DelegateCommand _Command_Delete_adressa;
+        public ICommand Button_Delete_adressa
+        {
+            get
+            {
+                if (_Command_Delete_adressa == null)
+                {
+                    _Command_Delete_adressa = new DelegateCommand(Execute_Delete_adressa, CanExecute_Delete_adressa);
+                }
+                return _Command_Delete_adressa;
+            }
+        }
+        private void Execute_Delete_adressa(object o)
+        {
+
+            list_adressa.Remove(select_item_adressa);
+
+            OnPropertyChanged(nameof(List_adressa));
+            Select_item_adressa = null;
+            Adressa = null;
+
+
+        }
+        private bool CanExecute_Delete_adressa(object o)
+        {
+
+
+            if (select_item_adressa != null)
+                return true;
+            else
+                return false;
+
+
+        }
+        #endregion
+        #region 
+        private DelegateCommand _Command_Edit_adressa;
+        public ICommand Button_Edit_adressa
+        {
+            get
+            {
+                if (_Command_Edit_adressa == null)
+                {
+                    _Command_Edit_adressa = new DelegateCommand(Execute_Edit_adressa, CanExecute_Edit_adressa);
+                }
+                return _Command_Edit_adressa;
+            }
+        }
+        private void Execute_Edit_adressa(object o)
+        {
+
+            Select_item_adressa.Name = Adressa;
+            OnPropertyChanged(nameof(List_adressa));
+
+
+        }
+        private bool CanExecute_Edit_adressa(object o)
+        {
+
+            if (select_item_adressa != null && adressa.Length > 0)
+                return true;
+            else
+                return false;
+
+
+        }
+        #endregion
+        #endregion Adressa
+
+        #region boss
+        #region 
+        private DelegateCommand _Command_New_boss;
+        public ICommand Button_New_boss
+        {
+            get
+            {
+                if (_Command_New_boss == null)
+                {
+                    _Command_New_boss = new DelegateCommand(Execute_New_boss, CanExecute_New_boss);
+                }
+                return _Command_New_boss;
+            }
+        }
+        private void Execute_New_boss(object o)
+        {
+
+            Boss temp = new Market.Boss();
+            temp.Name = Convert.ToString(name_boss);
+            temp.Surname = Convert.ToString(surname_boss);
+            list_boss.Add(temp);
+            OnPropertyChanged(nameof(List_boss));
+
+        }
+        private bool CanExecute_New_boss(object o)
+        {
+
+
+            if (name_boss != null && name_boss.Length > 0
+                && surname_boss != null && surname_boss.Length > 0)
+                return true;
+            else
+                return false;
+
+
+        }
+        #endregion
+        #region 
+        private DelegateCommand _Command_Delete_boss;
+        public ICommand Button_Delete_boss
+        {
+            get
+            {
+                if (_Command_Delete_boss == null)
+                {
+                    _Command_Delete_boss = new DelegateCommand(Execute_Delete_boss, CanExecute_Delete_boss);
+                }
+                return _Command_Delete_boss;
+            }
+        }
+        private void Execute_Delete_boss(object o)
+        {
+
+            list_boss.Remove(select_item_boss);
+
+            OnPropertyChanged(nameof(List_boss));
+            Select_item_boss = null;
+            Name_boss = null;
+            Surname_boss = null;
+
+
+        }
+        private bool CanExecute_Delete_boss(object o)
+        {
+
+
+            if (select_item_boss != null)
+                return true;
+            else
+                return false;
+
+
+        }
+        #endregion
+        #region 
+        private DelegateCommand _Command_Edit_boss;
+        public ICommand Button_Edit_boss
+        {
+            get
+            {
+                if (_Command_Edit_boss == null)
+                {
+                    _Command_Edit_boss = new DelegateCommand(Execute_Edit_boss, CanExecute_Edit_boss);
+                }
+                return _Command_Edit_boss;
+            }
+        }
+        private void Execute_Edit_boss(object o)
+        {
+
+            Select_item_boss.Name = Name_boss;
+            Select_item_boss.Surname = Surname_boss;
+            OnPropertyChanged(nameof(List_boss));
+
+
+        }
+        private bool CanExecute_Edit_boss(object o)
+        {
+
+            if (select_item_boss != null && name_boss.Length > 0 && surname_boss.Length > 0)
+                return true;
+            else
+                return false;
+
+
+        }
+        #endregion
+        #endregion Adressa
 
         #endregion Command button
 
@@ -286,6 +604,76 @@ namespace Market.ViewModel
         }
 
         #endregion City
+
+
+        #region Country
+
+        string country;
+        public string Country
+        {
+            set
+            {
+                country = value;
+                OnPropertyChanged(nameof(Country));
+            }
+            get
+            {
+                return country;
+            }
+        }
+
+        #endregion Country
+
+        #region Adressa
+
+        string adressa;
+        public string Adressa
+        {
+            set
+            {
+                adressa = value;
+                OnPropertyChanged(nameof(Adressa));
+            }
+            get
+            {
+                return adressa;
+            }
+        }
+
+        #endregion Adressa
+
+        #region Boss
+
+        string name_boss;
+        public string Name_boss
+        {
+            set
+            {
+                name_boss = value;
+                OnPropertyChanged(nameof(Name_boss));
+            }
+            get
+            {
+                return name_boss;
+            }
+        }
+
+
+        string surname_boss;
+        public string Surname_boss
+        {
+            set
+            {
+                surname_boss = value;
+                OnPropertyChanged(nameof(Surname_boss));
+            }
+            get
+            {
+                return surname_boss;
+            }
+        }
+
+        #endregion Boss
 
         #endregion Pole
 
@@ -373,6 +761,127 @@ namespace Market.ViewModel
         }
         #endregion Phone
 
+
+        #region Country
+        List<Country> list_country = new List<Market.Country>();
+        public ICollection<Country> List_country
+        {
+            set
+            {
+                list_country = value.ToList();
+                OnPropertyChanged(nameof(List_country));
+            }
+            get
+            {
+                return list_country.ToList();
+            }
+        }
+
+
+        Country select_item_country = new Country();
+        public Country Select_item_country
+        {
+            set
+            {
+                select_item_country = value;
+                OnPropertyChanged(nameof(Select_item_country));
+                Set_country();
+            }
+            get
+            {
+                return select_item_country;
+            }
+        }
+
+
+        void Set_country()
+        {
+            if (select_item_country != null)
+                Country = select_item_country.Name.ToString();
+        }
+        #endregion Country
+
+
+        #region Adressa
+        List<Adressa> list_adressa = new List<Market.Adressa>();
+        public ICollection<Adressa> List_adressa
+        {
+            set
+            {
+                list_adressa = value.ToList();
+                OnPropertyChanged(nameof(List_adressa));
+            }
+            get
+            {
+                return list_adressa.ToList();
+            }
+        }
+
+
+        Adressa select_item_adressa = new Adressa();
+        public Adressa Select_item_adressa
+        {
+            set
+            {
+                select_item_adressa = value;
+                OnPropertyChanged(nameof(Select_item_adressa));
+                Set_adressa();
+            }
+            get
+            {
+                return select_item_adressa;
+            }
+        }
+
+
+        void Set_adressa()
+        {
+            if (select_item_adressa != null)
+                Adressa = select_item_adressa.Name.ToString();
+        }
+        #endregion Adressa
+
+        #region Boss
+        List<Boss> list_boss = new List<Market.Boss>();
+        public ICollection<Boss> List_boss
+        {
+            set
+            {
+                list_boss = value.ToList();
+                OnPropertyChanged(nameof(List_boss));
+            }
+            get
+            {
+                return list_boss.ToList();
+            }
+        }
+
+
+        Boss select_item_boss = new Boss();
+        public Boss Select_item_boss
+        {
+            set
+            {
+                select_item_boss = value;
+                OnPropertyChanged(nameof(Select_item_boss));
+                Set_boss();
+            }
+            get
+            {
+                return select_item_boss;
+            }
+        }
+
+
+        void Set_boss()
+        {
+            if (select_item_boss != null)
+            {
+                Name_boss = select_item_boss.Name.ToString();
+                Surname_boss = select_item_boss.Surname.ToString();
+            }
+        }
+        #endregion Boss
 
         #endregion List and select
 
