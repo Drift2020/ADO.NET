7ce6dc1f16a0bf1399f7a305ca4df9ef60af9a4c
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-09-04 12:18:47 +0300

    disin

--- systems/Work4/Work4/App.xaml.cs
+++ systems/Work4/Work4/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Market.CODES;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +15,27 @@ namespace Work4
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            PresentationTraceSources.DataBindingSource.Listeners.Add(new BindingErrorTraceListener());
+            PresentationTraceSources.DataBindingSource.Switch.Level = SourceLevels.Error;
+        }
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+
+
+
+
+
+            MainWindow view = new MainWindow();
+
+            ViewModel.View_Model_Main viewModel = new ViewModel.View_Model_Main();
+            view.DataContext = viewModel;
+
+
+
+            view.ShowDialog();
+
+        }
     }
 }
