4bcc1f105f60bc8e4a20fc6fe68d68d4bbb418e7
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-09-05 14:51:56 +0300

    new qwest

---
+++ taskMeneg/WpfApp1/WpfApp1/ViweModel/Viwe_Model_Index.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.ViweModel
+{
+    class Viwe_Model_Index
+    {
+    }
+}
